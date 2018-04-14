224641c27a3b5ef5aef0fbdd89967e5571ae34d1
Gordon DuQuqesnay <gduquesnay@gmail.com>
2018-04-15 05:05:29 +0900

    Remove bash and some some other overly specific config

--- personal_installer.sh
+++ personal_installer.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash -o ~/.git-completion.bash
-curl https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh -o ~/.git-prompt.sh
-
 git clone https://github.com/gduquesnay/dotfiles.git ~/.dotfiles
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/vundle
 
@@ -12,10 +9,5 @@ ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
-ln -s ~/.dotfiles/bash_profile ~/.bash_profile
-ln -s ~/.dotfiles/irbrc ~/.irbrc
 ln -s ~/.dotfiles/gemrc ~/.gemrc
-ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/ackrc ~/.ackrc
-
